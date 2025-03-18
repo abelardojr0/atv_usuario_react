5742318be286603431edca03a85601f3cca6708b
Abelardo Júnior <abelardo.junior.181@gmail.com>
2025-03-18 14:42:12 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./layout";
+import { Home } from "./pages/Home";
+import { Cadastro } from "./pages/Cadastro";
+import { Page404 } from "./pages/Page404";
+import { Edicao } from "./pages/Edicao";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="edicao/:id" element={<Edicao />} />
+          <Route path="*" element={<Page404 />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
